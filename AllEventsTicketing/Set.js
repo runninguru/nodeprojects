ad61a95ba33ece346370bc0fe55e55d766cec978
Kyle Birch <mysecureview@kylebirch.info>
2017-09-19 22:22:10 -0600

    almost finished lessson 1

---
+++ AllEventsTicketing/Set.js
@@ -0,0 +1,93 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	    if(listA === null || listB === null){
+	    	return null;
+		}
+
+		for(var i = 0;i < listA.length;i++){
+	    	for(var j = 0;j < listB.length;j++){
+	    		if(listB[j] === listA[i]){
+	    			resultList.push(listB[j]);
+	    			break;
+				}
+			}
+		}
+
+
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+        if(listA === null || listB === null){
+            return null;
+        }
+
+        resultList = listA.concat(listB);
+        resultList = resultList.slice().sort();
+        for(var i = 0;i < resultList.length - 1;i++){
+        	if(resultList[i] === resultList[i + 1]){
+        		//remove only the first matching item from our comparison.
+        		resultList.splice(i, 1);
+			}
+		}
+	   /*-------------------------------Insert your code here -------------------------------------*/ 
+	   
+	   return resultList;
+	}
+
+
+	this.relativeComplement = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+        if(listA === null || listB === null){
+            return null;
+        }
+        resultList = listA.slice();
+        var symd = resultList.filter(function(x){
+            if(listA.indexOf(x) === -1 || (listA.indexOf(x) !== -1 && listB.indexOf(x) !== -1)){
+                resultList.splice(x, 1);
+            }
+        })
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+
+
+
+	this.symmetricDifference = function(listA, listB) {
+
+	   var resultList = [];
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+        if(listA === null || listB === null){
+            return null;
+        }
+        var tempArr = listA.concat(listB);
+        var symd = tempArr.filter(function(x){
+        	if(listA.indexOf(x) === -1 || listB.indexOf(x) === -1){
+        		resultList.push(x);
+			}
+		})
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   return resultList;
+	}	
+	
+
+}
